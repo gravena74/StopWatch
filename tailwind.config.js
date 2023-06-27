4ef9fb85e67fd9b658848159489ad543f4c07962
João Gravena <67060504+gravena74@users.noreply.github.com>
2023-06-27 16:41:07 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      width: {
+        '100': '100vw',
+      },
+
+      height: {
+        '100': '91.5vh',
+      },
+    },
+  },
+  plugins: [],
+}
